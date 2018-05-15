81ac40856e4d2cfe9d1afcd44de347b56fbef82b
bangyan.chen[陳邦彥] <bangyan.chen@104.com.tw>
2018-05-15 16:18:24 +0800

    aaa

---
+++ Utils.js
@@ -0,0 +1,26 @@
+Utils = 
+{
+    setGlobalVariable :
+    function( key, value )
+    {
+        pm.globals.set( key, value );
+    },
+    
+    getGlobalVariable :
+    function( key )
+    {
+        pm.globals.get( key );
+    },
+
+    setEnvironmentVariable :
+    function( key, value )
+    {
+        pm.environment.set( key, value );
+    },
+
+    getEnvironmentVariable : 
+    function( key )
+    {
+        pm.environment.get( key );
+    }
+} 
