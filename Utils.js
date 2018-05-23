e3634a0b668eb7287dd83c07068f3e94a05bc6ed
bangyan.chen[陳邦彥] <bangyan.chen@104.com.tw>
2018-05-23 17:26:31 +0800

    www

--- Utils.js
+++ Utils.js
@@ -33,6 +33,42 @@ Utils =
     setVariable : 
     function( key, value )
     {
-        return pm.variables.set( key, value );
+        pm.variables.set( key, value );
+    },
+
+    getValueFromJsonString :
+    function( jsonString, key )
+    {
+        let value;
+        JSON.parse( jsonString, 
+            ( property, data ) => { 
+                if ( property === key )
+                {
+                   value = data;
+                }                
+                return data;
+            } 
+        );
+        return value;
+    },
+
+    getValueObjectFromJsonString :
+    function( jsonString, ...keys )
+    {
+        let vo = {};
+        for ( let i = 0; i < keys.length; i++ )
+        {
+            vo[ keys[ i ] ] = "";
+        }
+        JSON.parse( jsonString, 
+            ( property, data ) => { 
+                if ( vo.hasOwnProperty( property ) )
+                {
+                   vo[ property ] = data;
+                }                
+                return data;
+            } 
+        );
+        return vo;
     }
 }; 
