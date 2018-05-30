a23d038cbe682b0fc0e4aabd205c73576dd3d05c
bangyan.chen[陳邦彥] <bangyan.chen@104.com.tw>
2018-05-30 09:43:30 +0800

    eee

---
+++ Selector.js
@@ -0,0 +1,10 @@
+Selector = 
+{
+    selectHttpStatus( context ) 
+    {
+        let statusText = context.statusText.replace( / /g, "_" ).toLowerCase();
+        let expectedCode = context.expectedCode;
+
+        return "expect_" + statusText + "_" + expectedCode;
+    }
+}
