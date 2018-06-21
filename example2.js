3e31861c8204631d6ed11382c40a04974a9586c2
E104TW\bangyan.chen <bangyan.chen@104.com.tw>
2018-06-21 17:02:25 +0800

    new example2

---
+++ example2.js
@@ -0,0 +1,28 @@
+eval( pm.variables.get( "pm_common" ) );
+
+class Tester extends TestTemplate
+{
+    setUp()
+    {
+        super.setUp();
+        // doSomething();
+    }
+
+    expect_ok_200()
+    {   
+        // main test
+    }
+
+    unexpected()
+    {
+        super.unexpected();
+        // moreExceptionHandle();
+    }
+
+    tearDown()
+    {
+        // totally overwrite
+    }
+}
+
+new Tester( new TestContext() ).run();
