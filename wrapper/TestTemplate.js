349f93f872ac6fb9812927b520f6cd8316bfa1f0
bangyan <bangyan.chen@104.com.tw>
2018-08-01 03:36:12 +0800

    update

--- wrapper/TestTemplate.js
+++ wrapper/TestTemplate.js
@@ -21,49 +21,11 @@ class TestTemplate
                          .addTestResult( `Response time : ${ cxt.responseTime } ms`, cxt.responseTime <= this.expectedResponseTime );
 	}
 
-	// run() {
-	// 	try {
-	// 		this.setUp();
-            
-	// 		while ( this.selector.hasNext() ) 
-	// 		{   
-	// 			let calleeName = this.selector.next(  this );
-
-    //             if ( this.isCallableFunction( calleeName ) ) 
-    //             {
-    //                 console.log( "Executing : " + calleeName);
-    //                 this[ calleeName ]();
-    //             }
-    //             else {
-    //                 console.log( "unexpected condition : no matched method");
-    //                 this.unexpected();
-    //             }
-    //         }
-    //     }
-    //     catch (error) {
-    //         const errMsg = `${ error.name } : ${ error.message }`;
-    //         console.log(errMsg);       
-    //         this.testReporter.fail( errMsg );
-    //     }
-    //     finally {
-    //         this.tearDown();
-    //         if ( this.testReporter )
-    //         {   
-    //             this.testReporter.results();
-    //         }   
-    //     }
-    // }
-
 	tearDown() {
 		console.log( "tear down : " + this.context.requestName );
 		if ( this.context.autoClear )
 		{
 			this.context.clearAttributes();
 		}
-	}
-
-	isCallableFunction( calleeName )
-	{
-		return ( calleeName in this ) && ( typeof this[ calleeName ] === "function" );
 	}
 }
